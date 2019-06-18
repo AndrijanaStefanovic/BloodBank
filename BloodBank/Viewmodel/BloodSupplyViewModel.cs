51509a002692a2ca82d05f01195296b2624156b8
andrijana.stefanovic <anja.stef@gmail.com>
2019-06-18 23:28:52 +0200

    BloodSupplyViewModel

---
+++ BloodBank/Viewmodel/BloodSupplyViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BloodBank.Model;
+using BloodBank.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Viewmodel
+{
+    public class BloodSupplyViewModel
+    {
+        public BloodSupply BloodSupply { get; set; }
+
+        public BloodSupplyViewModel()
+        {
+            BloodSupply = new BloodSupply(new DatabaseHandler());
+        }
+    }
+}
